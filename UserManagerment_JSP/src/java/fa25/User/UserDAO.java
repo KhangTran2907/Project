063d67e26d875d292e4158f8835c52cd6c77a8d5
KhangTran2907 <khangtranquoc2907@gmail.com>
2025-10-04 13:37:49 +0700

    Add files via upload

---
+++ UserManagerment_JSP/src/java/fa25/User/UserDAO.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fa25.User;
+
+import fa25.utils.DBUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author MSII
+ */
+public class UserDAO {
+    private static final String LOGIN = "SELECT fullName, roleID FROM tblUsers WHERE userID=? AND password=? ";
+    private static final String SEARCH = "SELECT userID, fullName, roleID FROM tblUsers WHERE fullName like ? ";
+    private static final String DELETE = "DELETE tblUsers WHERE userID=? ";
+    
+    public UserDTO checkLogin(String userID, String password) throws SQLException {
+    UserDTO user = null;
+    Connection conn = null;
+    PreparedStatement ptm = null;
+    ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(LOGIN);
+                ptm.setString(1, userID);
+                ptm.setString(2, password);
+                rs = ptm.executeQuery();
+                if (rs.next()) {
+                    String fullName = rs.getString("fullName");
+                    String roleID = rs.getString("roleID");
+                    user = new UserDTO(userID, fullName, roleID, "***");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) rs.close();
+            if (ptm != null) ptm.close();
+            if (conn != null) conn.close();      
+        }
+        return user;
+    }
+
+    public List<UserDTO> getListUser(String search) throws SQLException {
+        List<UserDTO> listUser = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+        try{
+            conn = DBUtils.getConnection();
+            if(conn!=null){
+                ptm= conn.prepareStatement(SEARCH);
+                ptm.setString(1, "%"+search+ "%");
+                rs= ptm.executeQuery();
+                while(rs.next()){
+                    String userID= rs.getString("userID");
+                    String fullName= rs.getString("fullName");
+                    String roleID= rs.getString("roleID");
+                    String password= "***";
+                    listUser.add(new UserDTO(userID, fullName, roleID, password));
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            if(rs!= null) rs.close();
+            if(ptm!= null) ptm.close();
+            if(conn!= null) conn.close();
+        }
+        return listUser;
+    }
+
+    public boolean delete(String userID) throws ClassNotFoundException, SQLException {
+       boolean check = false;
+       Connection conn = null;
+       PreparedStatement ptm = null;
+       try{
+           conn = DBUtils.getConnection();
+           if(conn!= null){
+               ptm= conn.prepareStatement(DELETE);
+               ptm.setString(1, userID);
+               check = ptm.executeUpdate()>0?true:false;
+           }
+       }catch (Exception e){
+           e.printStackTrace();
+       }finally {
+           if(ptm!= null) ptm.close();
+           if(conn!= null) conn.close();
+       }
+       return check;
+    }
+    
+}
+
+
